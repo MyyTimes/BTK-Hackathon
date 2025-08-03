dc9767c9bd57984000ca1f7d8b4b5a04bf7ceaa9
Muhammed Yusuf <77261036+MyyTimes@users.noreply.github.com>
2025-08-03 13:13:46 +0300

    Float değer input olarak verilebilir

--- lesson_functions.py
+++ lesson_functions.py
@@ -1,27 +1,27 @@
 # MAXIMUM QUESTIONS
-MAX_TURKCE_QUESTIONS = 20
-MAX_MATEMATIK_QUESTIONS = 20 
-MAX_FEN_QUESTIONS = 20
-MAX_INKILAP_QUESTIONS = 10
-MAX_DIN_QUESTIONS = 10
-MAX_INGILIZCE_QUESTIONS = 10
-TOTAL_QUESTIONS = 90
+MAX_TURKCE_QUESTIONS = 20.0
+MAX_MATEMATIK_QUESTIONS = 20.0 
+MAX_FEN_QUESTIONS = 20.0
+MAX_INKILAP_QUESTIONS = 10.0
+MAX_DIN_QUESTIONS = 10.0
+MAX_INGILIZCE_QUESTIONS = 10.0
+TOTAL_QUESTIONS = 90.0
 
 def get_max_question_number():
     return {"Matematik": MAX_MATEMATIK_QUESTIONS, "Turkce" : MAX_TURKCE_QUESTIONS, "Fen" : MAX_FEN_QUESTIONS, "Inkılap" : MAX_INKILAP_QUESTIONS, "Din Kulturu" : MAX_DIN_QUESTIONS, "Ingilizce" : MAX_INGILIZCE_QUESTIONS}
 
 def get_default_lesson_datas():
     lesson_labels = ["Türkçe", "Matematik", "Fen", "İnkılap", "Din Kültürü", "İngilizce", "Toplam"]
-    success_percentages = [0, 0, 0, 0, 0, 0, 0]
+    success_percentages = [.0, .0, .0, .0, .0, .0, .0]
     return lesson_labels, success_percentages
 
 def empty_exam_result_row():
     deneme_labels = ["Error"]
-    turkce_score = [0]
-    matematik_score = [0]
-    fen_score = [0]
-    inkilap_score = [0]
-    din_score = [0]
-    ingilizce_score = [0]
-    total_score = [0] 
+    turkce_score = [.0]
+    matematik_score = [.0]
+    fen_score = [.0]
+    inkilap_score = [.0]
+    din_score = [.0]
+    ingilizce_score = [.0]
+    total_score = [.0] 
     return (deneme_labels, turkce_score, matematik_score, fen_score, inkilap_score, din_score, ingilizce_score, total_score)    
